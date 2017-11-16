57a461febca266a8c26d0db621ef2996b9687824
Szczyprz <szczyprz95@gmail.com>
2017-11-16 13:35:54 +0100

    KOKO NIGHT ON  TUESDAY

--- complex.py
+++ complex.py
@@ -2,64 +2,88 @@
 
 import unittest
 
+
 class Complex(object):
-	def __init__(self, real, imag=0.0):
-		self.real = real
-		self.imag = imag
+    def __init__(self, real, imag=0.0):
+        self.real = real
+        self.imag = imag
 
-	def __add__(self, other):
-		return Complex(self.real + other.real, self.imag + other.imag)
-	
-	def __sub__(self, other):
-	        return Complex(self.real - other.real,
-        	               self.imag - other.imag)
+    def __add__(self, other):
+        return Complex(self.real + other.real, self.imag + other.imag)
 
-	def __mul__(self, other):
-		return Complex(self.real*other.real - self.imag*other.imag,
-               			 self.imag*other.real + self.real*other.imag)
+    def __sub__(self, other):
+        return Complex(self.real - other.real,
+                       self.imag - other.imag)
 
-	def __truediv__(self, other):
-        	r = float(other.real**2 + other.imag**2)
-        	return Complex((self.real*other.real+self.imag*other.imag)/r, (self.imag*other.real-self.real*other.imag)/r)
+    def __mul__(self, other):
+        return Complex(self.real * other.real - self.imag * other.imag,
+                       self.imag * other.real + self.real * other.imag)
 
-	def __abs__(self):
-        	return sqrt(self.real**2 + self.imag**2)
+    def __div__(self, other):
+        r = float(other.real ** 2 + other.imag ** 2)
+        return Complex((self.real * other.real + self.imag * other.imag) / r,
+                       (self.imag * other.real - self.real * other.imag) / r)
 
-	def __neg__(self):   # defines -c (c is Complex)
-        	return Complex(-self.real, -self.imag)
+    def __abs__(self):
+        return sqrt(self.real ** 2 + self.imag ** 2)
 
-	def __eq__(self, other):
-        	return self.real == other.real and self.imag == other.imag
+    def __neg__(self):  # defines -c (c is Complex)
+        return Complex(-self.real, -self.imag)
+
+    def __eq__(self, other):
+        return self.real == other.real and self.imag == other.imag
+
+    def __str__(self):
+        return '(%g, %g)' % (self.real, self.imag)
 
 
-	def __str__(self):
-        	return '(%g, %g)' % (self.real, self.imag)
-
-a = Complex(1,2)
-b = Complex(-5,-3)
-c = Complex(1,-5)
-d = Complex(-3,-4)
+a = Complex(1, 2)
+b = Complex(-5, -3)
+c = Complex(1, -5)
+d = Complex(-3, -4)
 e = 2
+
+z = Complex(2,3)
+g = Complex(1,8)
+h = Complex(3,4)
 
 
 class TestComplexMethods(unittest.TestCase):
-	
-	def testInit(self):
-		self.assertEqual(c.real,1)
-		self.assertEqual(c.imag,-5)
-	def testAdd(self):
-		f = a + b
-		self.assertEqual(f.real,-4)
-		self.assertEqual(f.imag,-1)
-	def testSub(self):
-		f = a - b
-		self.assertEqual(f.real,6)
-		self.assertEqual(f.imag,5)
-	def testMul(self):
-		f = a * b 
-		self.assertEqual(f.real,1)
-		self.assertEqual(f.imag,-13)
-	
+    def testInit(self):
+        self.assertEqual(c.real, 1)
+        self.assertEqual(c.imag, -5)
+
+    def testAdd(self):
+        f = a + b
+        self.assertEqual(f.real, -4)
+        self.assertEqual(f.imag, -1)
+
+    def testSub(self):
+        f = a - b
+        self.assertEqual(f.real, 6)
+        self.assertEqual(f.imag, 5)
+
+    def testMul(self):
+        f = a * b
+        self.assertEqual(f.real, 1)
+        self.assertEqual(f.imag, -13)
+
+    def testTruediv(self):
+        f = g / z
+        self.assertEqual(f.real, 2)
+        self.assertEqual(f.imag, 1)
+
+    def testNeg(self):
+        f = -h
+        self.assertEqual(f.real, -3)
+        self.assertEqual(f.imag, -4)
+
+    def testeq(self):
+        f = h
+        self.assertEqual(f.real, 3)
+        self.assertEqual(f.imag, 4)
+
+
 
 if __name__ == '__main__':
     unittest.main()
